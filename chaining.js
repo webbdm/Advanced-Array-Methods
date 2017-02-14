cef3a9bc4c12e06b07710acb9fc5a3ed8c119d60
webbdm <gwebb762@gmail.com>
2017-02-13 19:49:20 -0600

    exercise

--- chaining.js
+++ chaining.js
@@ -5,5 +5,11 @@
 // 3.  Multiply each remaining number by 1.5 and then subtract 1.
 // 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
-var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+var integer = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
+
+// 1.
+
+var chainingResult = integer.sort(function(a,b){return b-a}).filter(function(num){return num < 19});
+
+console.log("Result ", chainingResult); 
