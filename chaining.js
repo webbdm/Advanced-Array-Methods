c81bdfee95f1ba9ff185c6879bac6cc8b1af1617
webbdm <gwebb762@gmail.com>
2017-02-13 19:25:14 -0600

    notes

---
+++ chaining.js
@@ -0,0 +1,9 @@
+// Using one single line of JavaScript code, complete the following tasks on the array of integers below.
+
+// 1.  Sort the numbers in descending order (10, 9, 8, 7, etc).
+// 2.  Remove any integers greater than 19.
+// 3.  Multiply each remaining number by 1.5 and then subtract 1.
+// 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
+
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
---
+++ notes.js
@@ -0,0 +1,86 @@
+var fruit = ["apple", "banana", "cherry"];
+
+function outputFruitOld (array) {
+
+	for (var i = 0; i < array.length; i++) {
+		console.log(array[i]);
+	}
+
+}
+// // }
+
+// // outputFruitOld(fruits);
+
+
+// // function outputFruits (element, index, array) {
+// // 	console.log(element);
+// // 	console.log("array[" + index + "]" + array[i]);}
+
+// // fruits.forEach(outputFruits);
+
+
+// // fruits.forEach(function(fruit){
+// // 	console.log(fruit);
+
+// // });
+
+
+// // function backwardFruitsOld(array) {
+// // 	var backwardsFruit = [];
+// // 	for (var i = 0; i < array.length; i++) {
+// // 		backwardsFruit.push(array[i].split("").reverse().join(""));
+// // 	}
+// // 	console.log(backwardsFruit);
+// // }
+
+// // backwardFruitsOld(fruits);
+
+// // var backwardsFruit = fruits.map(function(fruit){
+// // 	return fruit.split("").reverse().join("");
+
+// // });
+// // console.log(backwardsFruit);
+
+
+
+
+
+
+
+// function fiveLettersOnlyOld (array){
+// 	var filteredFruits = [];
+// 	for (var i = 0; i < array.length; i++) {
+// 	   if(array[i].length === 5){
+// 		 filteredFruits.push(array[i]);
+// 	   }	
+// 	}
+// 	console.log(filteredFruits);
+// }
+
+// fiveLettersOnlyOld(fruit);
+
+
+// function filterIt (x){
+// 	if (x.length === 5) {
+// 		return x;
+// 	}
+// }
+
+
+
+// var numbers = [0,1,2,3,4];
+
+// function addzOld(array){
+// 	var sum = 0;
+// 	for (var i = 0; i < array.length; i++){
+// 		sum = sum + array[i];	
+// 	}
+// 	console.log(sum);
+// }
+// addzOld(numbers);
+
+// var sum = [0,1,2,3,4].reduce
+
+
+
+
